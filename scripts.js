086ef134cae3db5933f5a527f9a919c054bccb2c
Alois Sečkár <ellrohir@seznam.cz>
2022-12-30 19:26:43 +0100

    re-worked area object and way of working with it

--- scripts.js
+++ scripts.js
@@ -3,30 +3,30 @@
 let player = {
 	color: "#0066FF",
 	area: [
-		[240,240],
-		[280,240],
-		[280,280],
-		[240,280],
+		{ x: 240, y: 240, options: [[239,240],[240,239]] },
+		{ x: 280, y: 240, options: [[281,240],[280,239]] },
+		{ x: 280, y: 280, options: [[281,280],[280,281]] },
+		{ x: 240, y: 280, options: [[239,280],[240,281]] },
 	]
 };
 
 let pc1 = {
 	color: "#FF0000",
 	area: [
-		[40,40],
-		[80,40],
-		[80,80],
-		[40,80],
+		{ x: 40, y: 40, options: [[39,40],[40,39]] },
+		{ x: 80, y: 40, options: [[81,40],[80,39]] },
+		{ x: 80, y: 80, options: [[81,80],[80,81]] },
+		{ x: 40, y: 80, options: [[39,80],[40,81]] },
 	]
 };
 
 let pc2 = {
 	color: "#FFCC00",
 	area: [
-		[440,440],
-		[480,440],
-		[480,480],
-		[440,480],
+		{ x: 440, y: 440, options: [[439,440],[440,439]] },
+		{ x: 480, y: 440, options: [[481,440],[480,439]] },
+		{ x: 480, y: 480, options: [[481,480],[480,481]] },
+		{ x: 440, y: 480, options: [[439,480],[440,481]] },
 	]
 };
 
@@ -89,13 +89,13 @@ function repaintMap() {
 function repaintPlayerArea(ctx, color, area) {
 	ctx.fillStyle = color;
 	ctx.beginPath();
-	ctx.moveTo(area[0][0],area[0][1]);
+	ctx.moveTo(area[0].x,area[0].y);
 	index = 1;
 	while (index < area.length) {
-		ctx.lineTo(area[index][0],area[index][1]); 
+		ctx.lineTo(area[index].x,area[index].y); 
 		index += 1;
 	} 
-	ctx.lineTo(area[0][0],area[0][1]);
+	ctx.lineTo(area[0].x,area[0].y);
 	ctx.closePath();
 	ctx.fill();
 }
@@ -109,28 +109,26 @@ function expandAll() {
 /* expand player's area  */
 /* somewhere on the "edge" of it */
 function expand(entity) {
-	let rand_vertex = 0;
-	let options = [];
-	while (options.length < 1) {
-		// randomly select vertex to be expanded
-		rand_vertex = getRandomVertex(entity);
- 		// get available expansions (0-3)
-		options = analyzePoint(entity.area[rand_vertex][0], entity.area[rand_vertex][1]);
-		// if 0 options: vertex got enclosed by others => remove it
-		if (options.length < 1) {
-			entity.area.splice(rand_vertex, 1)
-		}
+	// randomly select vertex to be expanded
+	const rand_vertex = getRandomExpandableVertex(entity);
+	if (rand_vertex && rand_vertex.options.length > 0) {
+		// radomly select one of available
+		const expansion = Math.round(Math.random() * (rand_vertex.options.length - 1));
+		// expand map in selected direction
+		rand_vertex.x = rand_vertex.options[expansion][0];
+		rand_vertex.y = rand_vertex.options[expansion][1];
+		// analyze further expansion options
+		rand_vertex.options = analyzePoint(rand_vertex.x, rand_vertex.y);
+	} else {
+		// TODO handle stop expanding when out of options
 	}
-	
-	const expansion = Math.round(Math.random() * (options.length - 1)); // radomly select one of available
-	entity.area[rand_vertex] = options[expansion]; // expand map in selected direction
 }
 
 /* randomly select vertex of player's area */
 /* either random existing one or split longest line to create new */
-function getRandomVertex(entity) {
-	// decide between selecting and splitting (1% chance) // TODO test ideal probability...
-	if (Math.random()>0.99) {
+function getRandomExpandableVertex(entity) {
+	// decide between selecting and splitting (0.1% chance) // TODO test ideal probability...
+	if (Math.random()>0.999) {
 		const area = entity.area
 		const longest = { 
 			length: -1,
@@ -142,8 +140,8 @@ function getRandomVertex(entity) {
 			const i1 = index;
 			const i2 = index < area.length - 1 ? index + 1 : 0;
 			
-			const a = area[i1][0] - area[i1][0];
-			const b = area[i1][1] - area[i2][1];
+			const a = area[i1].x - area[i1].x;
+			const b = area[i1].y - area[i2].y;
 			const c = Math.sqrt( a*a + b*b );
 
 			if (c > longest.length) {
@@ -154,83 +152,80 @@ function getRandomVertex(entity) {
 		}
 		// split it in half
 		// get the midpoint
-		const x_mid = Math.round((area[longest.from][0] + area[longest.to][0]) / 2);
-		const y_mid = Math.round((area[longest.from][1] + area[longest.to][1]) / 2);
-		// create new point
-		const newPoint = [x_mid, y_mid];
+		const x_mid = Math.round((area[longest.from].x + area[longest.to].x) / 2);
+		const y_mid = Math.round((area[longest.from].y + area[longest.to].y) / 2);
+		// create new vertex
+		const newVertex = {
+			x: x_mid, 
+			y: y_mid,
+			options: analyzePoint(x_mid, y_mid) // TODO won't work if no options...
+		}
 		// insert it into existing vertexes (what, where)
-		insertMapCoord(entity, newPoint, longest.from);
+		insertMapCoord(entity, newVertex, longest.from);
 		//
-		return longest.from + 1;
+		return entity.area[longest.from + 1];
 	} else {
 		// just select existing vertex
-		return Math.round(Math.random() * (entity.area.length - 1));
+		const expandableVertices = entity.area.filter(v => v.options.length > 0);
+		if (expandableVertices) {
+			const rand = Math.round(Math.random() * (entity.area.length - 1));
+			return expandableVertices[rand];
+		} else {
+			return null;
+		}
 	}
 }
 
 /* check for given point neighbours */
-/* find those that are available for expansion (not red) */
+/* find those that are available for expansion (still green) */
 function analyzePoint(x, y) {
-	const results = [];
-	// check "east"
+	const options = [];
+	// check "left"
 	if (getPixelColor(x-1,y)=="006600") {
-		results.push([x-1,y]);
+		options.push([x-1,y]);
 	}
-	// check "west"
+	// check "right"
 	if (getPixelColor(x+1,y)=="006600") {
-		results.push([x+1,y]);
+		options.push([x+1,y]);
 	}
-	// check "north"
+	// check "up"
 	if (getPixelColor(x,y-1)=="006600") {
-		results.push([x,y-1]);
+		options.push([x,y-1]);
 	}
-	// check "south"
+	// check "down"
 	if (getPixelColor(x,y+1)=="006600") {
-		results.push([x,y+1]);
+		options.push([x,y+1]);
 	}
 	//
-	return results;
+	return options;
 }
 
 /* get current color of given pixel */
 /* in hex format */
 function getPixelColor(x, y) {
-	var canvas = document.getElementById("World");
-	var ctx = canvas.getContext('2d', { willReadFrequently: true });
+	var canvas = document.getElementById('World');
+	var ctx = canvas.getContext('2d');
 	// get color of pixel
 	data = ctx.getImageData(x, y, 1, 1).data;
 	// build hex string from extracted data
 	return data[0].toString(16).padStart(2, '0') + data[1].toString(16).padStart(2, '0') + data[2].toString(16).padStart(2, '0');
 }
 
-/* insert given point into existing map coordinates */
 /* create new vertex AFTER given index */
-function insertMapCoord(entity, point, index) {
+function insertMapCoord(entity, newVertex, index) {
 	if (index == entity.area.length - 1) {
 		// just append the element to the end
-		entity.area.push(point);
+		entity.area.push(newVertex);
 	} else {
 		// new array must be created from:
 		// part until index (including) + new element at index+1 + rest of orig array
 		arr1 = entity.area.slice(0, index + 1);
-		arr2 = [point];
+		arr2 = [newVertex];
 		arr3 = entity.area.slice(index + 1);
 		//
 		entity.area = arr1.concat(arr2, arr3);
 	}
 }
-
-/* draw random pixel (test purposes) */
-/*
-function drawPixel() {
-	var canvas = document.getElementById("World");
-	var ctx = canvas.getContext('2d', { willReadFrequently: true });
-	ctx.fillStyle = "#FF0000";
-	var x = Math.floor((Math.random() * canvas.width) + 1); // random x-coord
-	var y = Math.floor((Math.random() * canvas.height) + 1); // random y-coord
-	ctx.fillRect( x, y, 5, 5 ); // draw "pixel"
-}
-*/
 
 function countScore() {
 	let currentScore = '';
@@ -246,10 +241,10 @@ function calcArea(vertices) {
     var total = 0;
 
     for (var i = 0, l = vertices.length; i < l; i++) {
-      var addX = vertices[i][0];
-      var addY = vertices[i == vertices.length - 1 ? 0 : i + 1][1];
-      var subX = vertices[i == vertices.length - 1 ? 0 : i + 1][0];
-      var subY = vertices[i][1];
+      var addX = vertices[i].x;
+      var addY = vertices[i == vertices.length - 1 ? 0 : i + 1].y;
+      var subX = vertices[i == vertices.length - 1 ? 0 : i + 1].x;
+      var subY = vertices[i].y;
 
       total += (addX * addY * 0.5);
       total -= (subX * subY * 0.5);
